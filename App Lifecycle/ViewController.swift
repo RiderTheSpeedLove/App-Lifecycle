e98375b77819159c553f4e96377f9fab3e81e448
Sanjeev Dinesh Tiwari <Sanjeev4.Tiwari@ril.com>
2022-08-30 15:12:06 +0530

    Initial Commit

---
+++ App Lifecycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App Lifecycle
+//
+//  Created by Sanjeev4 Tiwari on 30/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
